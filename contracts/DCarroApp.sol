355be1d57e94e7aaae969c6cc4a0d048969fcb51
Tze Wei <tzeweiwee@gmail.com>
2022-03-28 01:16:55 +0800

    added core functions and unit tests

--- contracts/DCarroApp.sol
+++ contracts/DCarroApp.sol
@@ -1,6 +1,60 @@
 // SPDX-License-Identifier: unlicensed
 pragma solidity ^0.8.0;
 
+struct CarAdvert {
+    uint256 id;
+    string title;
+    string make;
+    string model;
+    uint256 price;
+    string imageUrl;
+}
+
 contract DCarroApp {
-  
+    address public owner;
+    string userName;
+    string userCreatedDate;
+    string userLocation;
+    string userContact;
+
+    uint256[] private carIds;
+    mapping(uint256 => CarAdvert) public cars;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner access");
+        _;
+    }
+
+    function setUserData(
+        string memory _userName,
+        string memory _userLocation,
+        string memory _userContact
+    ) public onlyOwner {
+        userName = _userName;
+        userLocation = _userLocation;
+        userContact = _userContact;
+    }
+
+    function getCars() public view returns (CarAdvert[] memory) {
+        CarAdvert[] memory carsArray = new CarAdvert[](carIds.length);
+        for (uint256 i = 0; i < carIds.length; i++) {
+            carsArray[i] = cars[carIds[i]];
+        }
+
+        return carsArray;
+    }
+
+    function getUserName() public view returns (string memory) {
+      return userName;
+    }
+    function getUserLocation() public view returns (string memory) {
+      return userLocation;
+    }
+    function getUserContact() public view returns (string memory) {
+      return userContact;
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,19 +1,29 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe('DCarroApp', function () {
+  it('Owner should set user data', async function () {
+    const Contract = await ethers.getContractFactory('DCarroApp');
+    const contract = await Contract.deploy();
+    await contract.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const setUserDataTx = await contract.setUserData(
+      'Eric',
+      'Pluto',
+      '0132228888'
+    );
 
     // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await setUserDataTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await contract.getUserName()).to.equal('Eric');
+    expect(await contract.getUserLocation()).to.equal('Pluto');
+    expect(await contract.getUserContact()).to.equal('0132228888');
+
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    await expect(
+      contract.connect(addr1).setUserData('Heimdall', 'Asgard', '0132222222')
+    ).to.be.revertedWith('Only owner access');
   });
 });
